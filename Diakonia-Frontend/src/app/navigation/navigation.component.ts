c7ba569cd663d3f53d1168ff10a2c324fd69ec94
STEVEN2199 <stevenguillermo_encalada@hotmail.com>
2023-11-08 12:52:21 -0500

    cambios
    cambios
    
    Co-Authored-By: Ricardo David Salazar Secaira <56646823+ricdasal@users.noreply.github.com>

--- Diakonia-Frontend/src/app/navigation/navigation.component.ts
+++ Diakonia-Frontend/src/app/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Emitters } from '../emitters/emitters';
 import { HttpClient } from '@angular/common/http';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -16,6 +17,7 @@ export class NavigationComponent implements OnInit {
 
   constructor(
     private http: HttpClient,
+    private router: Router
   ){}
 
   menuVariable:boolean = false;
@@ -32,9 +34,11 @@ export class NavigationComponent implements OnInit {
 
   logout(): void {
     this.http.post('http://localhost:8000/api/logout', {}, {withCredentials:true})
-    .subscribe( () => this.authenticated = false);
-  }
-
+    .subscribe( () => {
+        this.authenticated = false;
+        this.router.navigate(['/login']);
+    });
+}
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
