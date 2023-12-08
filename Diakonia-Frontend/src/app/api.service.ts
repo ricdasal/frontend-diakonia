3485485a7089821bf987e59cd6e30497a0907cf1
trolencio_777@outlook.es <dterrero@espol.edu.ec>
2023-12-07 22:07:31 -0500

    resolucion de conflictos de peticiones

--- Diakonia-Frontend/src/app/api.service.ts
+++ Diakonia-Frontend/src/app/api.service.ts
@@ -1,6 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const headers = new HttpHeaders({
+  Authorization: `Bearer ${localStorage.getItem("ACCESS_TOKEN")}`
+})
 @Injectable({
   providedIn: 'root'
 })
@@ -9,27 +12,27 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   AllInstituciones(){
-    return this.http.get<any>("http://localhost:8000/api/AllInstituciones");
+    return this.http.get<any>("http://localhost:8000/api/AllInstituciones", {headers, withCredentials: true});
   }
 
   postInstitucion(data:any){
-    return this.http.post<any>("http://localhost:8000/api/postInstitucion", data);
+    return this.http.post<any>("http://localhost:8000/api/postInstitucion", data, {headers, withCredentials: true});
   }
 
   DataInstituciones(){
-    return this.http.get<any>("http://localhost:8000/api/DataInstituciones");
+    return this.http.get<any>("http://localhost:8000/api/DataInstituciones", {headers, withCredentials: true});
   }
 
   DataUsers(){
-    return this.http.get<any>("http://localhost:8000/api/DataUsers");
+    return this.http.get<any>("http://localhost:8000/api/DataUsers", {headers, withCredentials: true});
   }
 
   DataInstitucionesId(id: string){
-    return this.http.get<any>(`http://localhost:8000/api/DataInstituciones/${id}`);
+    return this.http.get<any>(`http://localhost:8000/api/DataInstituciones/${id}`, {headers, withCredentials: true});
   }
 
   DataInstitucionesDirecciones(){
-    return this.http.get<any>(`http://localhost:8000/api/DataInstitucionesDirecciones`);
+    return this.http.get<any>(`http://localhost:8000/api/DataInstitucionesDirecciones`, {headers, withCredentials: true});
   }
 }
 
