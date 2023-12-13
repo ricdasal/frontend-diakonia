ea65a7e70588d74be669d43606adefc9ab1d00ea
deivizz21 <trolencio_777@outlook.es>
2023-12-12 23:33:55 -0500

    Correccion de Editar Usuarios

--- Diakonia-Frontend/src/app/modal-admin-users/modal-admin-users.component.ts
+++ Diakonia-Frontend/src/app/modal-admin-users/modal-admin-users.component.ts
@@ -1,58 +1,69 @@
-import { Component, OnInit, Inject  } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
-import {NgIf, NgFor} from '@angular/common';
+import { Component, OnInit, Inject } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
 import { ApiService } from '../api.service';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-admin-users',
   templateUrl: './modal-admin-users.component.html',
-  styleUrls: ['./modal-admin-users.component.css']
+  styleUrls: ['./modal-admin-users.component.css'],
 })
-export class ModalAdminUsersComponent implements OnInit{
-
-  usersForm !: FormGroup;
+export class ModalAdminUsersComponent implements OnInit {
+  usersForm!: FormGroup;
   actionBtn: string = 'Save';
 
-  constructor(private formbuilder: FormBuilder,
+  constructor(
+    private formbuilder: FormBuilder,
     private api: ApiService,
-    @Inject(MAT_DIALOG_DATA) public editData : any,
-    private dialogRef: MatDialogRef<ModalAdminUsersComponent>){}
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private dialogRef: MatDialogRef<ModalAdminUsersComponent>
+  ) {}
 
   ngOnInit(): void {
-      this.usersForm = this.formbuilder.group({
-        id:['',Validators.required],
-        name:['',Validators.required],
-        apellido:['',Validators.required],
-        email: ['',Validators.required],
-        cargo_institucional:['',Validators.required],
-        telefono:['',Validators.required],
-        password: ['', Validators.required]
-      });
-      if(this.editData){
-        this.usersForm.patchValue(this.editData);
-      }
-
+    this.usersForm = this.formbuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      apellido: ['', Validators.required],
+      email: ['', Validators.required],
+      cargo_institucional: ['', Validators.required],
+      telefono: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    if (this.editData) {
+      this.usersForm.patchValue(this.editData);
+    }
   }
 
-  editUser(){
-    if(this.usersForm.valid){
+  editUser() {
+    if (this.usersForm.valid) {
       this.api.editUser(this.usersForm.value, this.editData.id).subscribe({
-        next:(res: any)=>{
-          alert("Usuario editado exitosamente");
+        next: (res: any) => {
+          alert('Usuario editado exitosamente');
           this.usersForm.reset();
-          this.dialogRef.close('edit');
+          this.dialogRef.close('update');
         },
-        error:()=>{
-          alert("Error al editar usuario");
-        }
-      })
-    } 
+        error: () => {
+          alert('Error al editar usuario');
+        },
+      });
+    }
   }
 
   toppings = new FormControl('');
 
-  toppingList: string[] = ['Administrador', 'Usuario General', 'Usuario Invitado'];
+  toppingList: string[] = [
+    'Administrador',
+    'Usuario General',
+    'Usuario Invitado',
+  ];
 
   email = new FormControl('', [Validators.required, Validators.email]);
 
@@ -63,5 +74,4 @@ export class ModalAdminUsersComponent implements OnInit{
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
-
 }
